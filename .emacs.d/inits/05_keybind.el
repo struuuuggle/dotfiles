aff2bde5fe63124863a5e554ab95918a4095459d
struuuuggle <s15ti032@gmail.com>
2017-09-30 12:31:15 +0900

    Added a reference URL

--- .emacs.d/inits/05_keybind.el
+++ .emacs.d/inits/05_keybind.el
@@ -6,6 +6,7 @@
 (setq mac-option-modifier 'meta)
 
 ;; C-hにbackspaceと同じ処理を割り当てる
+;; ref. http://akisute3.hatenablog.com/entry/20120318/1332059326
 (global-set-key "\C-h" 'delete-backward-char)
 
 ;;; 05_keybind.el ends here
