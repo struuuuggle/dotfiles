49280c9e9dabff570753db047055c8a0c2cbf749
struuuuggle <s15ti032@gmail.com>
2017-10-20 01:15:24 +0900

    Updated powerline-my-theme mainly

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,4 +1,4 @@
-
+;;; init.el --- for general
 ;;; Commentary:
 ;;; Code:
 
@@ -27,20 +27,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(inhibit-startup-screen t)
  '(initial-frame-alist (quote ((top . 23) (left . 2))))
  '(package-selected-packages
    (quote
     (dashboard init-loader powerline ## atom-one-dark-theme package-utils atom-dark-theme rainbow-delimiters flycheck color-theme-sanityinc-solarized)))
  '(tab-always-indent t)
  '(tab-width 4))
-
-;; ウィンドウサイズ
-(setq default-frame-alist
-      (append (list
-               '(width . 100)
-               '(height . 80)
-               )
-              default-frame-alist))
 
 ;; *.~ とかのバックアップファイルを作らない
 (setq make-backup-files nil)
@@ -50,18 +43,6 @@
 
 ;; カーソル行をハイライトする
 (global-hl-line-mode t)
-
-;;for Ricty
-(add-to-list 'default-frame-alist '(font . "ricty-15"))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-
-;;for flycheck
-(global-flycheck-mode)
 
 ;;透明度
 (set-frame-parameter nil 'alpha 90)
@@ -77,56 +58,10 @@
 ;;(setq eol-mnemonic-mac "(CR)")
 ;;(setq eol-mnemonic-unix "(LF)")
 
-;;for Powerline
-(require 'powerline)
-
-(defun powerline-my-theme ()
-  "Setup the my mode-line."
-  (interactive)
-  (setq powerline-current-separator 'utf-8)
-  (setq-default mode-line-format
-                '("%e"
-                  (:eval
-                   (let* ((active (powerline-selected-window-active))
-                          (mode-line (if active 'mode-line 'mode-line-inactive))
-                          (face1 (if active 'mode-line-1-fg 'mode-line-2-fg))
-                          (face2 (if active 'mode-line-1-arrow 'mode-line-2-arrow))
-                          (separator-left (intern (format "powerline-%s-%s"
-                                                          (powerline-current-separator)
-                                                          (car powerline-default-separator-dir))))
-                          (lhs (list (powerline-raw " " face1)
-                                     (powerline-major-mode face1)
-                                     (powerline-raw " " face1)
-                                     ;;(funcall separator-left face1 face2)
-                                     (powerline-buffer-id nil )
-                                     ;;(powerline-raw " [ ")
-                                     ;;(powerline-raw mode-line-mule-info nil)
-                                     ;;(powerline-raw "%*" nil)
-                                     ;;(powerline-raw " |")
-                                     ;;(powerline-process nil)
-                                     ;;(powerline-vc)
-                                     ;;(powerline-raw " ]")
-                                     ))
-                          (rhs (list ;;(powerline-raw "%4l" 'l)
-                                     ;;(powerline-raw ":" 'l)
-                                     (powerline-raw "ch=%2c" 'l)
-                                     (powerline-raw " | ")
-                                     (powerline-raw "%6p" )
-                                     (powerline-raw " ")
-                                     )))
-                     (concat (powerline-render lhs)
-                             (powerline-fill nil (powerline-width rhs))
-                             (powerline-render rhs)))))))
-
-(defun make/set-face (face-name fg-color bg-color weight)
-  (make-face face-name)
-  (set-face-attribute face-name nil
-                      :foreground fg-color :background bg-color :box nil :weight weight))
-(make/set-face 'mode-line-1-fg "#282C34" "#EF8300" 'bold)
-(make/set-face 'mode-line-2-fg "#AAAAAA" "#2F343D" 'bold)
-(make/set-face 'mode-line-1-arrow  "#AAAAAA" "#3E4451" 'bold)
-(make/set-face 'mode-line-2-arrow  "#AAAAAA" "#3E4451" 'bold)
-
-(powerline-my-theme)
-
 ;;; init.el ends here
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
