3958df93744905436c0bb92777aa2d10bf408f90
Mikiya Abe <s15ti032@gmail.com>
2018-02-08 02:13:47 +0900

    fish swims in my shell

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -13,3 +13,5 @@ ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.pyenv ~/.pyenv
 ln -sf ~/dotfiles/.rbenv ~/.rbenv
 ln -sf ~/dotfiles/.spacemacs ~/.spacemacs
+ln -sf ~/fisherman/ ~/.config/fisherman
+ln -sf ~/dotfiles/fisherman/ ~/.config/fisherman
