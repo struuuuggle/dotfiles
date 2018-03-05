c78b97158f3d0031dacbe7e82bbe8e49c1cbb86c
Mikiya Abe <s15ti032@gmail.com>
2018-03-06 00:19:06 +0900

    Add install.sh

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Usage: sh install.sh
+
+for f in .??*
+do
+    # symlinkを貼りたくないファイルを以下に書いておく
+    [[ "$f" == ".git" ]] && continue
+    [[ "$f" == ".gitignore" ]] && continue
+    [[ "$f" == ".DS_Store" ]] && continue
+    [[ "$f" == ".emacs.d.bak" ]] && continue
+
+    echo "$f"
+done
