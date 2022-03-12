795ee0c56ea607b2e68fcd238aadb042d053a529
struuuuggle <struuuuggle@gmail.com>
2022-03-12 21:15:55 +0900

    fix: fail to load zsh-autosuggestions

--- install.sh
+++ install.sh
@@ -65,4 +65,10 @@ do
     make_symlink $(realpath $f)
 done
 
+if [ ! -d ~/.zsh/zsh-autosuggestions ]; then
+    git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+fi
+
+source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
+
 echo "✅ Done"
