b482fa887514d20a104a9b20059b05cbb0051e14
struuuuggle <mikiya.abe@mixi.co.jp>
2025-02-09 19:50:16 +0900

    fix: stop profiler as default

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -7,13 +7,13 @@
 
 ;; https://zenn.dev/zk_phi/books/cba129aacd4c1418ade4/viewer/a53ba0ad0d729886a1dc
 (require 'profiler)
-(profiler-start 'cpu)
+; (profiler-start 'cpu)
 
 (require 'org)
 (defvar my-config-dir (concat user-emacs-directory "mine/"))
 (org-babel-load-file (expand-file-name "init.org" my-config-dir))
 
-(profiler-report)
-(profiler-stop)
+; (profiler-report)
+; (profiler-stop)
 
 ;;; init.el ends here
