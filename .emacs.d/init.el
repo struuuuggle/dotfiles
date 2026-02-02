721b9ad9f9d4ecfeae29b89685766a8f1c26dbc9
struuuuggle <struuuuggle@gmail.com>
2026-02-02 18:25:42 +0900

    perf: load pre-tangled init config

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -9,9 +9,27 @@
 (require 'profiler)
 ; (profiler-start 'cpu)
 
-(require 'org)
-(defvar my-config-dir (concat user-emacs-directory "mine/"))
-(org-babel-load-file (expand-file-name "init.org" my-config-dir))
+(defvar my-config-dir (expand-file-name "mine/" user-emacs-directory))
+(defvar my-config-org (expand-file-name "init.org" my-config-dir))
+(defvar my-config-el (expand-file-name "init.el" my-config-dir))
+(defvar my-config-elc (expand-file-name "init.elc" my-config-dir))
+
+(defun my/tangle-init-org ()
+  "Tangle init.org into init.el."
+  (interactive)
+  (require 'org)
+  (org-babel-tangle-file my-config-org my-config-el))
+
+(when (and (file-exists-p my-config-org)
+           (file-exists-p my-config-el)
+           (file-newer-than-file-p my-config-org my-config-el))
+  (message "init.org is newer than init.el; run M-x my/tangle-init-org to update."))
+
+(let ((config-to-load (if (and (file-exists-p my-config-elc)
+                               (file-newer-than-file-p my-config-elc my-config-el))
+                          my-config-elc
+                        my-config-el)))
+  (load config-to-load nil 'nomessage))
 
 ; (profiler-report)
 ; (profiler-stop)
